5cb26165d5ea48d2562c755faa565bc452af1267
rick_barkley <rick_barkley@nuvola-networks.com>
2014-03-17 13:48:28 -0500

    updated roles and ability to edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,19 +14,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def edit
-    @user = User.find(params[:id])
-  end
+  #def edit
+ #   @user = User.find(params[:id])
+ # end
   
   def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
-    params[:user].delete(:password) if params[:user][:password].blank?
-    params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
-    if @user.update_attributes(params[:user])
-      flash[:notice] = "Successfully updated User."
-      redirect_to root_path
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to users_path, :notice => "User updated."
     else
-      render :action => 'edit'
+      redirect_to users_path, :alert => "Unable to update user."
     end
   end
     
