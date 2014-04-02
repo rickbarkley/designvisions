809312206d7f4a59e572275862d24a042fe018e6
rick_barkley <rick_barkley@nuvola-networks.com>
2014-04-02 13:56:16 -0500

    updated with carrierwave gem

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -65,6 +65,16 @@ class Plan < ActiveRecord::Base
 
   mount_uploader :image, PortfolioUploader
   mount_uploader :image2, PortfolioUploader
+  mount_uploader :image3, PortfolioUploader  
+  mount_uploader :image4, PortfolioUploader  
+  mount_uploader :image5, PortfolioUploader 
+  mount_uploader :image6, PortfolioUploader
+  mount_uploader :image7, PortfolioUploader 
+  mount_uploader :image8, PortfolioUploader 
+  mount_uploader :image9, PortfolioUploader 
+  mount_uploader :image10, PortfolioUploader 
+  mount_uploader :image11, PortfolioUploader
+  mount_uploader :image12, PortfolioUploader
 
   has_many :users, through: :saved_plans
   has_many :saved_plans, :dependent => :destroy
